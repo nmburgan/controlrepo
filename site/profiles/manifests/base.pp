736d43201862cbe501a19d510404e4ee4f3cd4b9
Tim Meusel <tim@bastelfreak.de>
2023-07-29 20:03:25 +0200

    enhance tests

--- site/profiles/manifests/base.pp
+++ site/profiles/manifests/base.pp
@@ -58,6 +58,7 @@ class profiles::base (
       loglevel  => 'debug',
     },
   }
+  # ensure update runs before installing packages
   Class['apt::update'] -> Package <| provider == 'apt' |>
 
   # https://www.sshaudit.com/hardening_guides.html
